6a6478f2ec3b36bc38c1f1955371b1b61efc32e0
hoangdt04 <maivanhoang112k4@gmail.com>
2025-08-14 13:21:48 +0700

    Practice With ReactJS

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import React from "react";
-import Accordions from "./components/Accordions";
-import { accordionData } from "./utils/content";
+// import Accordions from "./components/Accordions";
+// import { accordionData } from "./utils/content";
+import Form from "./components/Form";
 
 const App = () => {
   return (
     <div>
-      <div className="accordion">
+      {/* <div className="accordion">
         {accordionData.map(({ title, content }) => (
           <Accordions title={title} content={content} />
         ))}
+      </div> */}
+
+      <div>
+        <Form />
       </div>
     </div>
   );
